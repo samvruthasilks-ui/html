bfa6d0a9ab3d74249ae56a03ad855d90b7e0b92c
samvruthasilks-ui <samvruthasilks@gmail.com>
2026-02-16 14:47:43 +0530

    change in FormToSheet & index.html

--- FormToSheet.js
+++ FormToSheet.js
@@ -1,27 +1,41 @@
-import react from 'react'
+import React from 'react';
 
-function FormToSHeet() {
-    //header: {  Contect-Type: application/x-www-form-urlencoded}'
+function FormToSheet() {
 
-const handleSubmit = (e)=>{
-    e.priventDefault()
-    const url = 'https://script.google.com/macros/s/AKfycbywobQ-W9O-LZ-mUkEnxrHwG6MgKpntNCXMbCKzYTCsjioycyA48jotsAx7D2qh7fdz8w/exec'
-    fetch(url,{
-        method: "POST",
-        headers: {" Content-Type": "application/x-www-form-urlencoded"}
-    body:(Gmail=${e.target.Gmail.value} &MobileNumber=${e.target.MobileNumber.value})
-}).then(res=>res.text()).then(data=>{
-    alert(data)
-}).catch(error=>console.log(error))
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const url = "https://script.google.com/macros/s/AKfycbywobQ-W9O-LZ-mUkEnxrHwG6MgKpntNCXMbCKzYTCsjioycyA48jotsAx7D2qh7fdz8w/exec";
+
+    const formData = new URLSearchParams();
+    formData.append("Gmail", e.target.gmail.value);
+    formData.append("MobileNumber", e.target.mobileNumber.value);
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: formData.toString()
+    })
+    .then(res => res.text())
+    .then(data => {
+      alert("Submitted Successfully!");
+      e.target.reset();
+    })
+    .catch(error => console.log(error));
+  };
+
+  return (
+    <div>
+      <h1>React to Sheet</h1>
+      <form onSubmit={handleSubmit}>
+        <input name="gmail" placeholder="Gmail" required /><br/>
+        <input name="mobileNumber" placeholder="Mobile Number" required /><br/>
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
 }
 
-    return(
-        <div>
-            <h1>react to sheet</h1>
-            <form  onSubmit= {handleSubmit}>
-                <input name= 'gmail ' placeholder='gmail'/><br/>
-                <input name = 'mobileNumber' placeholder='Mobile Number'/> <br/>
-                <button>Add</button>
-                </form>
-        </div>
-    )}
+export default FormToSheet;
